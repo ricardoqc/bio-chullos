30f92cf89bdbff41de7ae6aa6eeedb775b5ace6f
RicardoQC <ricardoqc@byzoop.com>
2024-08-20 13:51:53 -0500

    Se agrego el archivo .cpanel

--- src/assets/links/urls_wpp.js
+++ src/assets/links/urls_wpp.js
@@ -2,4 +2,4 @@ const cusco6dias = "https://api.whatsapp.com/send?phone=+54992558512&text=Hola+C
 const cusco5dias = "https://api.whatsapp.com/send?phone=+54992558512&text=Hola+Chullos+Tours%2C+estoy+interesado+en+el+paquete+de+Cusco+5+D%C3%ADas%2C+me+brindan+mas+informaci%C3%B3n%3F"
 const lagunahumantay = "https://api.whatsapp.com/send?phone=+54992558512&text=Hola+Chullos+Tours%2C+estoy+interesado+en+el+Tour+de+Laguna+Humantay+full+day%2C+me+brindan+mas+informaci%C3%B3n%3F"
 
-export {cusco6dias, cusco5dias, lagunahumantay};
+export { cusco6dias, cusco5dias, lagunahumantay };
